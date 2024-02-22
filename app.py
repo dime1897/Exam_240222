b9f2c17632efec04594d7b46d6c5cdff89891bb1
dime1897 <diego.mercoliano@live.com>
2024-02-22 15:47:31 +0100

    Punto1

--- app.py
+++ app.py
@@ -1,23 +1,73 @@
 from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 from wtforms import Form, IntegerField, StringField, validators, SubmitField
-from exam import Name_of_class
+from exam import UaaS
 
 app = Flask(__name__,
             static_url_path='/static', 
             static_folder='static')
-dao=Name_of_class()
+dao=UaaS()
 
 api = Api(app)
 basePath = '/api/v1'
+umarell_fields = ['nome', 'cognome', 'cap']
+cantiere_fields = ['indirizzo', 'cap']
 
-class Base_Handling(Resource):
-    def get(self):
-        pass
-    def post(self):
-        pass
-    def put(self):
-        pass
+def validate_cantiere(cantiere:dict) -> bool:
+    try:
+        if len(cantiere.keys()) != len(cantiere_fields): return False
+        for field in cantiere.keys():
+            if not field in cantiere_fields: return False
+        if not isinstance(cantiere['cap'], int) or cantiere['cap'] < 0 or cantiere['cap'] > 99999: return False
+        return True
+    except: return False
+
+def validate_umarell(umarell:dict) -> bool:
+    try:
+        if len(umarell.keys()) != len(umarell_fields): return False
+        for field in umarell.keys():
+            if not field in umarell_fields: return False
+        if not isinstance(umarell['cap'], int) or umarell['cap'] < 0 or umarell['cap'] > 99999: return False
+        return True
+    except: return False
+
+def validate_id(id:str):
+    try:
+        int(id)
+        return True
+    except: return False
+
+class Cantiere(Resource):
+    def get(self, id):
+        if not validate_id(id): return None, 404
+        cantiere = dao.get_cantiere(id)
+        if cantiere is None: return None, 404
+        return cantiere, 200
+    def post(self, id):
+        if not validate_id(id): return None, 400
+        cantiere = request.json
+        if not validate_cantiere(cantiere): return None, 400
+        conflict = dao.get_cantiere(id)
+        if not conflict is None: return None, 409
+        rc = dao.insert_cantiere(cantiere, id)
+        if rc is None: return None, 400
+        return cantiere, 201
+
+class Umarell(Resource):
+    def get(self, id):
+        if not validate_id(id): return None, 404
+        umarell = dao.get_umarell(id)
+        if umarell is None: return None, 404
+        return umarell, 200
+    def post(self, id):
+        if not validate_id(id): return None, 400
+        umarell = request.json
+        if not validate_umarell(umarell): return None, 400
+        conflict = dao.get_umarell(id)
+        if not conflict is None: return None, 409
+        rc = dao.insert_umarell(umarell, id)
+        if rc is None: return None, 400
+        return umarell, 201
 
 
 class Clean_DB(Resource):
@@ -26,8 +76,9 @@ class Clean_DB(Resource):
         return None, 200
     
 
-api.add_resource(Base_Handling, f'{basePath}/pathototheapi')
+api.add_resource(Cantiere, f'{basePath}/cantiere/<id>')
+api.add_resource(Umarell, f'{basePath}/umarell/<id>')
 api.add_resource(Clean_DB, f'{basePath}/clean')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run("""host='127.0.0.1', port=8080, debug=True""")
--- exam.py
+++ exam.py
@@ -1,14 +1,54 @@
 from google.cloud import firestore
 
-class Name_of_class(object):
+class UaaS(object):
     def __init__(self):
         self.db = firestore.Client()
     
     def clean_db(self):
-        ref = self.db.collection('name_of_collection')
+        ref = self.db.collection('cantieri')
+        ref2 = self.db.collection('umarell')
         try:
             for doc in ref.list_documents():
+                doc.delete()
+            for doc in ref2.list_documents():
                 doc.delete()
         except Exception as e:
             print("Exception cought in clean_db(): {" + str(e) + "}")
     
+    def insert_cantiere(self, cantiere:dict, id:str) -> dict:
+        ref = self.db.collection('cantieri')
+        try:
+            ref.document(id).set(cantiere)
+            return cantiere
+        except Exception as e:
+            print("Exception cought in insert_cantiere(): {" + str(e) + "}")
+            return None
+
+    def get_cantiere(self, id:str) -> dict:
+        ref = self.db.collection('cantieri')
+        try:
+            doc = ref.document(id).get()
+            cantiere = doc.to_dict() if doc.exists else None
+            return cantiere
+        except Exception as e:
+            print("Exception cought in get_cantiere(): {" + str(e) + "}")
+            return None
+        
+    def insert_umarell(self, umarell:dict, id:str) -> dict:
+        ref = self.db.collection('umarell')
+        try:
+            ref.document(id).set(umarell)
+            return umarell
+        except Exception as e:
+            print("Exception cought in insert_umarell(): {" + str(e) + "}")
+            return None
+
+    def get_umarell(self, id:str) -> dict:
+        ref = self.db.collection('umarell')
+        try:
+            doc = ref.document(id).get()
+            umarell = doc.to_dict() if doc.exists else None
+            return umarell
+        except Exception as e:
+            print("Exception cought in get_umarell(): {" + str(e) + "}")
+            return None
